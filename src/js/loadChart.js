272e2acbc84f03b3a968be64aaf1840a156f0eb6
HeroLeam <thiagoleam@gmail.com>
2024-05-31 09:11:09 -0300

    Update loadChart.js

--- src/js/loadChart.js
+++ src/js/loadChart.js
@@ -12,25 +12,30 @@ function carregarCartelas() {
       var parser = new DOMParser();
       var htmlDoc = parser.parseFromString(text, 'text/html');
       var files = Array.from(htmlDoc.querySelectorAll('a'))
-        .map(a => a.pathname)
-        .filter(href => href.endsWith('.html'))
+        .map(a => a.getAttribute('href')) // Use getAttribute para pegar o href corretamente
+        .filter(href => href && href.endsWith('.html') && href !== 'index.html') // Excluir index.html
         .sort((a, b) => {
           var matchA = a.match(/([A-Z]+)(\d+)/);
           var matchB = b.match(/([A-Z]+)(\d+)/);
-          var letterA = matchA[1];
-          var numberA = parseInt(matchA[2]);
-          var letterB = matchB[1];
-          var numberB = parseInt(matchB[2]);
 
-          if (letterA !== letterB) {
-            return letterA.localeCompare(letterB);
+          if (matchA && matchB) {
+            var letterA = matchA[1];
+            var numberA = parseInt(matchA[2]);
+            var letterB = matchB[1];
+            var numberB = parseInt(matchB[2]);
+
+            if (letterA !== letterB) {
+              return letterA.localeCompare(letterB);
+            } else {
+              return numberA - numberB;
+            }
           } else {
-            return numberA - numberB;
+            return a.localeCompare(b);
           }
         });
 
       files.forEach(file => {
-        fetch(window.location.origin + file)
+        fetch(window.location.origin + pasta + '/' + file) // Adicione a '/' entre pasta e file
           .then(response => response.text())
           .then(html => {
             var div = document.createElement('div');
